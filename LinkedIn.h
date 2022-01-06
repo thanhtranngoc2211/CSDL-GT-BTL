9e68b06e1ef09c7468d41ab922ee0918ac45fe86
thanhtranngoc2211 <83736518+thanhtranngoc2211@users.noreply.github.com>
2022-01-06 11:47:34 +0700

    Add DFS algorithm

--- LinkedIn.h
+++ LinkedIn.h
@@ -53,6 +53,7 @@ class LinkedIn {
   // Add edges
   void addEdge(std::string i, std::string j, std::vector<Node> a, Weight w) {
     int node_1 = -1, node_2 = -1;
+    string name_1, name_2;
     // Not add if input same node
     if (i == j){
       printf("Can't execute. Error: Same node\n");
@@ -60,16 +61,19 @@ class LinkedIn {
     }
     // Check id
     for (int ite = 0; ite < a.size(); ite++){
-      if ((i == a[ite].name || j == a[ite].name) && node_1 == -1){
-        node_1 = a[ite].id;
+      if ((i == a.at(ite).name || j == a.at(ite).name) && node_1 == -1){
+        node_1 = a.at(ite).id;
+        name_1 = a.at(ite).name;
       }
-      else if (i == a[ite].name || j == a[ite].name)
-        node_2 = a[ite].id;
+      else if (i == a.at(ite).name || j == a.at(ite).name)
+        node_2 = a.at(ite).id;
+        name_2 = a.at(ite).name;
     }
     // Check successfully, add
     if (node_1 != -1 && node_2 != -1){
       adjMatrix.at(node_1).at(node_2) = w;
       adjMatrix.at(node_2).at(node_1) = w;
+      cout << "Successfully add relation between" + name_1 + "and" + name_2<<endl;
     }
     else { 
       printf("Error");
@@ -78,17 +82,21 @@ class LinkedIn {
 
   // Remove edges
   void removeEdge(std::string i, std::string j, std::vector<Node>a) {
+    string name_1, name_2;
     int node_1 = -1, node_2 = -1;
     for (int ite = 0; ite < a.size(); ite++){
       if (i == a[ite].name && node_1 == -1){
         node_1 = a[ite].id;
+        name_1 = a.at(ite).name;
       }
       else
         node_2 = a[ite].id;
+        name_2 = a.at(ite).name;
     }
     if (node_1 != -1 && node_2 != -1){
       adjMatrix.at(node_1).at(node_2) = {-1,-1};
       adjMatrix.at(node_2).at(node_1) = {-1,-1};
+      cout << "Successfully delete relation between" + name_1 + "and" + name_2;
     }
   }
 
@@ -102,33 +110,55 @@ class LinkedIn {
     }
   }
 
-  // Search fellow
+  vector<int> dfs(std::vector<Node> a, int node_id, std::vector<bool> isChecked, vector<int> stack){
+    isChecked.at(node_id) = true;
+      for (int ite = 0; ite < a.size(); ite++){
+        if (stack.size() > a.size() - 2)
+          break;
+        if (adjMatrix.at(node_id).at(ite).years != -1 && !isChecked.at(a[ite].id)){
+          isChecked.at(a[ite].id) = true;
+          stack.push_back(a[ite].id);
+          stack = dfs(a, a[ite].id, isChecked, stack);
+        }
+    }
+    return stack;
+  }
+
   void searchFellow(std::vector<Node> a, std::string nameSearch){
-    std::string work;
-    std::vector<string> fellow;
-    // Search work
-    for (int i = 0; i < a.size(); i++){
-      if (nameSearch == a[i].name){
-        work = a[i].work;
+    std::vector<int> stack;
+    std::vector<bool> isChecked;
+    std::vector<string> fellows;
+    isChecked.assign(a.size(), false);
+    int node_id;
+    bool found = false;
+    // nameSearch id
+    for (int ite = 0; ite < a.size(); ite++){
+      if (a[ite].name == nameSearch){
+        node_id = a[ite].id;
+        found = true;
         break;
       }
     }
-    // If work together & nameSearch is not vector[i].name --> print out
-    for (int i = 0; i < a.size(); i++){
-      if (work == a[i].work && nameSearch != a[i].name){
-        fellow.push_back(a[i].name);
+    if (found == true){
+      stack = dfs(a, node_id, isChecked, stack);
+      // Search each node from stack
+      for (int ite = 0; ite < stack.size(); ite++){
+        if (a[node_id].work == a[stack.at(ite)].work){
+          fellows.push_back(a[stack.at(ite)].name);
+        }
       }
-    }
-    // No fellow found
-    if (fellow.size() == 0){
-      cout << "Not found any fellow\n";
+      if (fellows.size() == 0)
+        cout << "No fellow found" << endl;
+      else {
+        cout << "Fellows: ";
+        for (int ite =  0; ite < fellows.size(); ite++){
+          cout << fellows.at(ite) << " ";
+        }
+        cout << endl;
+      }
     }
     else {
-      cout << "Fellows: ";
-      for (int i = 0; i < fellow.size(); i++){
-        cout << fellow[i] + " ";
-      }
-      cout << endl;
+      printf("Not found name");
     }
   }
 
@@ -168,7 +198,7 @@ class LinkedIn {
       printf("Can't execute. Error: Same node\n");
       return;
     }
-    // Check id
+    // Search id
     for (int ite = 0; ite < a.size(); ite++){
       if ((i == a[ite].name || j == a[ite].name) && node_1 == -1){
         node_1 = a[ite].id;
