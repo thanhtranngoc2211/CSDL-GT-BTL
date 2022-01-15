d49a382927e2617533a912bcc2e38bacceefca85
thanhtranngoc2211 <83736518+thanhtranngoc2211@users.noreply.github.com>
2022-01-15 11:36:55 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -20,6 +20,7 @@
 #include "Login.h"
 #include "Weight.h"
 #include "Register.h"
+#include "data.h"
 using namespace std;
 
 int main() {
@@ -34,79 +35,12 @@ int main() {
   // Store mans
   std::vector<Node> a;
   Weight w;
-  int control = 9999;
+  int control;
   bool open = true, logIn = true;
 
-  g.addNode();
-  man.id = 0;
-  man.name = "thanh";
-  man.dob = "22/11/2000";
-  man.email = "thanhtranngoc2211@gmail.com";
-  man.number = "0822455477";
-  man.field = "AI";
-  man.work = "NAVER";
-  a.push_back(man);
-
-  g.addNode();
-  man.id = 1;
-  man.name = "linh";
-  man.dob = "10/11/2003";
-  man.email = "linh.ngu@gmail.com";
-  man.number = "0915273456";
-  man.field = "law";
-  man.work = "NAVER";
-  a.push_back(man);
-
-  g.addNode();
-  man.id = 2;
-  man.name = "huyen";
-  man.dob = "17/09/2000";
-  man.email = "huyen.dt@gmail.com";
-  man.number = "0335428632";
-  man.field = "marketing";
-  man.work = "PG";
-  a.push_back(man);
-
-  g.addNode();
-  man.id = 3;
-  man.name = "van";
-  man.dob = "30/10/1996";
-  man.email = "van@gmail.com";
-  man.number = "0915874352";
-  man.field = "home";
-  man.work = "NAVER";
-  a.push_back(man);
-
-  g.addNode();
-  man.id = 4;
-  man.name = "la";
-  man.dob = "1/10/2000";
-  man.email = "laviethai@gmail.com";
-  man.number = "0912514567";
-  man.field = "dev";
-  man.work = "lotus";
-  a.push_back(man);
-
-  w.papers = 10;
-  w.years = 18;
-  name_1 = "thanh";
-  name_2 = "linh";
-  g.addEdge(name_1,name_2,a,w);
-  w.papers = 15;
-  w.years = 3;
-  g.addEdge("thanh","huyen",a,w);
-  w.papers = 0;
-  w.years = 3;
-  g.addEdge("linh","van",a,w);
-  w.papers = 3;
-  w.years = 3;
-  g.addEdge("la","huyen",a,w);
-  w.papers = 0;
-  w.years = 3;
-  g.addEdge("thanh","van",a,w);
-  w.papers = 0;
-  w.years = 2;
-  g.addEdge("la","van",a,w);
+  // Load data
+  cout << "-----------Fetching Data----------" <<endl;
+  fetchData(g,a);
 
   while (open){
     cout << "-------------LinkedIn-------------" <<endl;
