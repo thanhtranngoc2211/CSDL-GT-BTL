650b2d49c1b6c3ca8c42897c2f12060c142d160c
thanhtranngoc2211 <83736518+thanhtranngoc2211@users.noreply.github.com>
2022-01-27 21:31:08 +0700

    Update Register.h

--- Register.h
+++ Register.h
@@ -5,20 +5,32 @@
 #include <vector>
 #include "Node.h"
 #include "LinkedIn.h"
+#include <fstream>
+#include <tuple>
 using namespace std;
 
-void register_user(vector<Node>a, LinkedIn g){
+std::tuple<vector<Node>, LinkedIn>register_user(vector<Node> a, LinkedIn g){
     Node man;
     g.addNode();
+    fstream outfile;
+    outfile.open("data.txt", ios::out | ios::app);
     cout << "Your name: "; std::getline(std::cin,man.name);
+    outfile << man.name << endl;
     cout << "Date of birth: "; std::getline(std::cin,man.dob);
+    outfile << man.dob << endl;
     cout << "Your email: "; std::getline(std::cin,man.email);
+    outfile << man.email << endl;
     cout << "Your field: "; std::getline(std::cin,man.field);
+    outfile << man.field << endl;
     cout << "Your number: "; std::getline(std::cin,man.number);
+    outfile << man.number << endl;
     cout << "Your work: "; std::getline(std::cin,man.work);
+    outfile << man.work << endl;
+    outfile.close();
     man.id = a.size();
     a.push_back(man);
     cout << "Register successfully";
+    return std::make_tuple(a,g);
 }
 
 
